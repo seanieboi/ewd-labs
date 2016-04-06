1ce4a0472978e7f1a2fd72af77578a00be3dfb37
Sean O'Connor <seanie.beg@gmail.com>
2016-04-06 19:35:25 +0100

    Second App added along with lab2 html

--- node-lab-1/app2.js
+++ node-lab-1/app2.js
@@ -1,17 +1,9 @@
 var express = require('express');
-//Add greeting module
-var greeting = require('./greeting');
 //create an express app
 var app = express();
 
 // Configure the app to serve up content from public directory
 app.use(express.static(__dirname + '/public'));
-
-//add route for /greeting 
-app.get('/greeting',function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end(greeting.english());
-});
 
 // Listen on port 8000, IP defaults to 127.0.0.1
 app.listen(8000)
